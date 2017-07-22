85813b3aceeeefc82ba98b6dcd9123d5d77d0605
Keith Lauher <klauher@cambriasolutions.com>
2017-07-22 09:04:17 -0700

    Update 2017-07-22T16:04:16.137Z

--- bundle.js
+++ bundle.js
@@ -1,73 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Test = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (global){
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-	value: true
-});
-
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var _react = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
-
-var _react2 = _interopRequireDefault(_react);
-
-var _commonInputFields = require('./common/inputFields');
-
-var _commonInputFields2 = _interopRequireDefault(_commonInputFields);
-
-var _commonTextArea = require('./common/textArea');
-
-var _commonTextArea2 = _interopRequireDefault(_commonTextArea);
-
-var Test = (function (_React$Component) {
-	_inherits(Test, _React$Component);
-
-	function Test() {
-		_classCallCheck(this, Test);
-
-		_get(Object.getPrototypeOf(Test.prototype), 'constructor', this).apply(this, arguments);
-	}
-
-	_createClass(Test, [{
-		key: 'render',
-		value: function render() {
-			return _react2['default'].createElement(
-				'div',
-				null,
-				_react2['default'].createElement('br', null),
-				_react2['default'].createElement(
-					'div',
-					null,
-					_react2['default'].createElement(_commonInputFields2['default'], null)
-				),
-				_react2['default'].createElement('br', null),
-				_react2['default'].createElement(
-					'div',
-					null,
-					_react2['default'].createElement(_commonTextArea2['default'], null)
-				)
-			);
-		}
-	}]);
-
-	return Test;
-})(_react2['default'].Component);
-
-exports['default'] = Test;
-module.exports = exports['default'];
-
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./common/inputFields":2,"./common/textArea":3}],2:[function(require,module,exports){
-(function (global){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -84,7 +15,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _react = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
@@ -140,9 +71,7 @@ var InputFields = (function (_React$Component) {
 exports['default'] = InputFields;
 module.exports = exports['default'];
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],3:[function(require,module,exports){
-(function (global){
+},{"react":undefined}],2:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -159,7 +88,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _react = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
@@ -214,6 +143,70 @@ var TextArea = (function (_React$Component) {
 exports['default'] = TextArea;
 module.exports = exports['default'];
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[1])(1)
+},{"react":undefined}],"react-woodduck":[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+	value: true
 });
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _commonInputFields = require('./common/inputFields');
+
+var _commonInputFields2 = _interopRequireDefault(_commonInputFields);
+
+var _commonTextArea = require('./common/textArea');
+
+var _commonTextArea2 = _interopRequireDefault(_commonTextArea);
+
+var Test = (function (_React$Component) {
+	_inherits(Test, _React$Component);
+
+	function Test() {
+		_classCallCheck(this, Test);
+
+		_get(Object.getPrototypeOf(Test.prototype), 'constructor', this).apply(this, arguments);
+	}
+
+	_createClass(Test, [{
+		key: 'render',
+		value: function render() {
+			return _react2['default'].createElement(
+				'div',
+				null,
+				_react2['default'].createElement('br', null),
+				_react2['default'].createElement(
+					'div',
+					null,
+					_react2['default'].createElement(_commonInputFields2['default'], null)
+				),
+				_react2['default'].createElement('br', null),
+				_react2['default'].createElement(
+					'div',
+					null,
+					_react2['default'].createElement(_commonTextArea2['default'], null)
+				)
+			);
+		}
+	}]);
+
+	return Test;
+})(_react2['default'].Component);
+
+exports['default'] = Test;
+module.exports = exports['default'];
+
+},{"./common/inputFields":1,"./common/textArea":2,"react":undefined}]},{},[]);
--- example.js
+++ example.js
@@ -1,3 +1,4 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -8,57 +9,41 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _react = require('react');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Test = require('react-woodduck');
 
-var _react2 = _interopRequireDefault(_react);
+var App = (function (_React$Component) {
+	_inherits(App, _React$Component);
 
-var _commonInputFields = require('./common/inputFields');
+	function App() {
+		_classCallCheck(this, App);
 
-var _commonInputFields2 = _interopRequireDefault(_commonInputFields);
-
-var _commonTextArea = require('./common/textArea');
-
-var _commonTextArea2 = _interopRequireDefault(_commonTextArea);
-
-var Test = (function (_React$Component) {
-	_inherits(Test, _React$Component);
-
-	function Test() {
-		_classCallCheck(this, Test);
-
-		_get(Object.getPrototypeOf(Test.prototype), 'constructor', this).apply(this, arguments);
+		_get(Object.getPrototypeOf(App.prototype), 'constructor', this).apply(this, arguments);
 	}
 
-	_createClass(Test, [{
+	_createClass(App, [{
 		key: 'render',
 		value: function render() {
-			return _react2['default'].createElement(
+			return React.createElement(
 				'div',
 				null,
-				_react2['default'].createElement('br', null),
-				_react2['default'].createElement(
-					'div',
-					null,
-					_react2['default'].createElement(_commonInputFields2['default'], null)
-				),
-				_react2['default'].createElement('br', null),
-				_react2['default'].createElement(
-					'div',
-					null,
-					_react2['default'].createElement(_commonTextArea2['default'], null)
-				)
+				React.createElement(Test, null)
 			);
 		}
 	}]);
 
-	return Test;
-})(_react2['default'].Component);
+	return App;
+})(React.Component);
 
-exports['default'] = Test;
+exports['default'] = App;
+;
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
 module.exports = exports['default'];
+
+},{"react":undefined,"react-dom":undefined,"react-woodduck":undefined}]},{},[1]);
